ad32d8da5bc665814d9cfdb938f302554af48cc1
AaronTweeton <aaron@aarontweeton.com>
2026-01-03 13:32:56 -0800

    Disallow core block patterns. Disallow loading remote block patterns.

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 add_action('after_setup_theme', 'rusch_park_setup');
 
+// Prevent loading of remote block patterns. 
+add_filter('should_load_remote_block_patterns', false);
+
 function rusch_park_setup()
 {
     add_theme_support(
@@ -17,4 +20,6 @@ function rusch_park_setup()
             'chat',
         )
     );
+    // Only allow theme-specific block patterns
+    remove_theme_support('core-block-patterns');
 }
